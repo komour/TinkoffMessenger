d5a9f8c90b31f36d5220baa9d521b46853d9ccb5
u-menya-lapki <komaroff404@gmail.com>
2020-02-16 22:06:36 +0300

    hw1 done

--- TinkoffMessenger/AppDelegate.swift
+++ TinkoffMessenger/AppDelegate.swift
@@ -14,23 +14,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        var window: UIWindow?
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        printLog("The launch process is initiated: application: \(#function)\n")
+
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        printLog("The launch process is nearly complete: \(#function)\n")
+        
+        return true
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    @available(iOS, deprecated: 13.0)
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        printLog("The application state changes from Inactive to Active: \(#function)\n")
+    }
+    
+    @available(iOS, deprecated: 13.0)
+    func applicationWillResignActive(_ application: UIApplication) {
+        printLog("The application state changes from Active to Inactive: \(#function)\n")
+    }
+    
+    @available(iOS, deprecated: 13.0)
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        printLog("The application state changes from Foreground (Inactive) to Background: \(#function)\n")
+    }
+    
+    @available(iOS, deprecated: 13.0)
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        printLog("The application state changes from Background to Foreground (Inactive): \(#function)\n")
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        printLog("Termination event has been triggered: \(#function)\n")
+    }
+    
+    func printLog(_ message : String) {
+        #if Logs
+            print(message)
+        #endif
     }
 
     // MARK: - Core Data stack
@@ -78,5 +103,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
+}
+
+
+@available(iOS 13.0, *)
+extension AppDelegate {
+    
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
 }
 
--- TinkoffMessenger/ViewController.swift
+++ TinkoffMessenger/ViewController.swift
@@ -10,11 +10,87 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        printLog("\(#function)\n")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        printLog("\(#function)\n")
+    }
+    
+    override func loadViewIfNeeded() {
+        super.loadViewIfNeeded()
+        
+        printLog("\(#function)\n")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        printLog("\(#function)\n")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        printLog("\(#function)\n")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        printLog("\(#function)\n")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        printLog("\(#function)")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        printLog("\(#function)\n")
+    }
+    
+    override func updateViewConstraints() {
+        printLog("\(#function)\n")
+        
+        super.updateViewConstraints()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        printLog("\(#function)\n")
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        printLog("\(#function)\n")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        printLog("\(#function)\n")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        printLog("\(#function)\n")
+    }
+    
+    
 
+    func printLog(_ message : String) {
+        #if Logs
+            print(message)
+        #endif
+    }
 
 }
 
