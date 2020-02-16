f1ba8f675ce0928a07546938ef485918d8f5fdf8
u-menya-lapki <komaroff404@gmail.com>
2020-02-16 21:23:01 +0300

    Initial Commit

---
+++ TinkoffMessenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinkoffMessenger
+//
+//  Created by Always Strive And Prosper on 16.02.2020.
+//  Copyright © 2020 komarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
