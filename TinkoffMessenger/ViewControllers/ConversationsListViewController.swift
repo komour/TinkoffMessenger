614c8e7b44d6c36e4b0bad256c6dab871ab9b514
komour <komaroff404@gmail.com>
2020-02-29 02:14:57 +0300

    files structure change, hw3 progress

---
+++ TinkoffMessenger/ViewControllers/ConversationsListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConversationsListViewController.swift
+//  TinkoffMessenger
+//
+//  Created by Always Strive And Prosper on 29.02.2020.
+//  Copyright © 2020 komarov. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
