9d4b78579992a97340209b81f8df534b63cdb7b4
komour <komaroff404@gmail.com>
2020-04-13 00:57:03 +0400

    initial refactor

---
+++ TinkoffMessenger/Presentation/Profile/ProfileModel.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileModel.swift
+//  TinkoffMessenger
+//
+//  Created by Always Strive And Prosper on 13.04.2020.
+//  Copyright © 2020 komarov. All rights reserved.
+//
+
+import Foundation
